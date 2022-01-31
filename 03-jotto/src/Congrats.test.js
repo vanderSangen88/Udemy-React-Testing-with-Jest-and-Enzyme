08a27d902b9fea48721a2817241bccaef5e1e4af
vanderSangen88 <nick.vandersangen@gmail.com>
2022-01-31 15:50:39 +0100

    S03 L41 - 43

--- 03-jotto/src/Congrats.test.js
+++ 03-jotto/src/Congrats.test.js
@@ -1,11 +1,7 @@
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { checkProps } from "../test/testUtils";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
-import { findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 

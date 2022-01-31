a553c1ce1ab410cb9f05a735794d6456d7cbe20c
vanderSangen88 <nick.vandersangen@gmail.com>
2022-01-31 13:28:26 +0100

    S03 L34 - 35

---
+++ 03-jotto/src/Congrats.js
@@ -0,0 +1,11 @@
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is falsy)
+ */
+const Congrats = ({ success }) => {
+  return <div />;
+};
+
+export default Congrats;

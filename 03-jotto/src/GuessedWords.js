975f761b13d923d615e0e16ca5505194977ab609
vanderSangen88 <nick.vandersangen@gmail.com>
2022-02-15 09:21:22 +0100

    progress S4 L49 - 50

--- 03-jotto/src/GuessedWords.js
+++ 03-jotto/src/GuessedWords.js
@@ -9,6 +9,27 @@ const GuessedWords = ({ guessedWords }) => {
           Try to guess the secret word!
         </span>
       )}
+      {guessedWords.length && (
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWords.map(({ guessedWord, letterMatchCount }, index) => (
+                <tr data-test="guessed-word" key={index}>
+                  <td>{guessedWord}</td>
+                  <td>{letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };

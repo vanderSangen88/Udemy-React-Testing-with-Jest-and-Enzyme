c3a78a8dc86d0da5d9577fdc69cd191df332af75
vanderSangen88 <nick.vandersangen@gmail.com>
2022-02-17 23:36:12 +0100

    progress S05 L53 - 60

---
+++ 03-jotto/src/Input.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Functional react component for input.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered container with input
+ */
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(event) => {
+            event.preventDefault();
+            // TODO: update guessedWords
+            // TODO: check against secretWord and update success if needed
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+Input.propTypes = { secretWord: PropTypes.string.isRequired };
+export default Input;
